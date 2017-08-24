60a3cd26ec0e915b4680559d9ccf64766ebf6ba3
dennismuturia <dmuturia2922@gmail.com>
2017-08-24 10:25:49 +0300

    Method getname added
    This method is used to retrieve the private parameter mname to be accessible in another class

--- src/main/java/Customer.java
+++ src/main/java/Customer.java
@@ -1,6 +1,11 @@
 public class Customer(){
+    //Create some class variables
+    private String mName;
 //We create the Customer method where the name string will be passed
     public Customer(String name){
-
+        mName = name
+    }
+    public getName(){
+        return mName;
     }
 }

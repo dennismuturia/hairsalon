7994fe492f11ec5b879f5b898cd450ae52936b28
dennismuturia <dmuturia2922@gmail.com>
2017-08-24 09:50:05 +0300

    Commented on a test file
    Just as the DB.java file,  this comment shows what the instance of the sql object does.

--- src/test/java/CustomerTest.java
+++ src/test/java/CustomerTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.*;
 public class CustomerTest{
     @Before
     public void setUp(){
+        //Pass the database information ie the password and the username of the DB as well as its localhost
         DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/hairsalon_test", null, null);
     }
 }

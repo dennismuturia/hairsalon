330be5fe68e84ea2e6787714fd1bda2de0d78abb
dennismuturia <dmuturia2922@gmail.com>
2017-08-24 09:48:19 +0300

    Commented in DB.java
    I wanted to show what does what in the code by adding a comment in the file

--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -1,5 +1,6 @@
 import org.sql2o.*;
 
 public class DB{
+    //Pass the database information ie the password and the username of the DB as well as its localhost
     public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/hairsalon", null, null);
 }

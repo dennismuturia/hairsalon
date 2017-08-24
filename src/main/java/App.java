11f350815cfea03e52df45b66732c89713a123cc
dennismuturia <dmuturia2922@gmail.com>
2017-08-24 08:22:18 +0300

    Initialization of the app
    This App java class will be the the one that will hold all our routes and manage them for us. I have created the layout of the App java class

---
+++ src/main/java/App.java
@@ -0,0 +1,13 @@
+//Making sure that i have all the  imports that i require in this file
+import java.util.Map;
+import java.util.HashMap;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+import static spark.Spark.*;
+
+public class App{
+    //this is the method or the main method where the templates will be rendered at
+     public static void main(String[] args) {
+        
+    }
+}

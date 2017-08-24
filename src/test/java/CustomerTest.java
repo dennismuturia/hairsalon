3349dfdc867dc33bb979225e1caae3db8046274e
dennismuturia <dmuturia2922@gmail.com>
2017-08-24 09:58:33 +0300

    Added the after block
    THis will be responsible of cleaning out our database apparently from the the customers table but in the hairsalon_test database which is the test database

--- src/test/java/CustomerTest.java
+++ src/test/java/CustomerTest.java
@@ -8,4 +8,13 @@ public class CustomerTest{
         //Pass the database information ie the password and the username of the DB as well as its localhost
         DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/hairsalon_test", null, null);
     }
+
+    @After
+    //This method Clears down the database tables
+    public void tearDown() {
+      try(Connection con = DB.sql2o.open()) {
+        String sql = "DELETE FROM customers *;";
+        con.createQuery(sql).executeUpdate();
+      }
+    }
 }

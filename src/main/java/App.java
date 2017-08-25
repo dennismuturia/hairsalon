74a54bedf5b9a59958ba97dd8f7c268a35335812
dennismuturia <dmuturia2922@gmail.com>
2017-08-25 18:30:34 +0300

    Madesome changes to the inetrface.
    My grale wasnt running on the interface, so i made some changes to my gradle.build. I also worked on the interface of my app. added some javascript and html to make it look great

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,6 +8,14 @@ import static spark.Spark.*;
 public class App{
     //this is the method or the main method where the templates will be rendered at
      public static void main(String[] args) {
+        staticFileLocation("/public");
+        String layout = "templates/layout.vtl";
+    
+        get("/", (request, response) -> {
+          Map<String, Object> model = new HashMap<String, Object>();
+          model.put("template", "templates/index.vtl");
+          return new ModelAndView(model, layout);
+        }, new VelocityTemplateEngine());
         
     }
 }
